78659dc97ef09a35759de63fdc715b4c83e4df27
Ben G <benjamingoumalatsos@lab2112pc18.ycstech.org>
2015-11-06 10:58:11 -0500

    Fixed Label Error
    Made changes so labels aren't packed every time the buttons are pressed.
    Now, the text of the labels change when the button is pressed.

--- main.py
+++ main.py
@@ -6,19 +6,25 @@ root.title("Idea Generator")
 
 rootCanvas = tkinter.Canvas(root, width = 200, height = 10)
 
+firstWord = [line.strip() for line in open("dictionary.txt")]
+wordOne = tkinter.Label(root, text = " ")
+
+secondWord = [line.strip() for line in open("operations.txt")]
+wordTwo = tkinter.Label(root, text = " ")
+
 def generate():
-    firstWord = [line.strip() for line in open("dictionary.txt")]
-    wordOne = tkinter.Label(root, text = random.choice(firstWord))
-
-    secondWord = [line.strip() for line in open("operations.txt")]
-    wordTwo = tkinter.Label(root, text = " " + random.choice(secondWord))
-
-    wordOne.pack()
-    wordTwo.pack()
+    global firstWord
+	global wordOne
+	global secondWord
+	global wordTwo
+	wordOne.config(text = random.choice(firstWord))
+	wordTwo.config(text = random.choice(secondWord))
 
 generateIdea = tkinter.Button(root, text = "Generate Idea", command = generate)
 
 generateIdea.pack()
+wordOne.pack()
+wordTwo.pack()
 rootCanvas.pack()
 
 root.mainloop()
